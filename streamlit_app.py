914b374ec1abfbf4c64e67d4bdec10e8b503593e
PPDATAWIZARD <135508093+PPDATAWIZARD@users.noreply.github.com>
2023-06-04 15:39:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 
-streamlit.header("Breakfast Menu")
+streamlit.header("FUCK YOU NIRAJ")
 streamlit.text("Omega 3 & blueberry oatmeal")
 streamlit.text("Kale, Spinach & Rocket Smoothie")
 streamlit.text("Hard-boiled Free-Range Egg")
