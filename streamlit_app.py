6c894f870c0cf02fc10b5ea1ad7f286a7238b16c
PPDATAWIZARD <135508093+PPDATAWIZARD@users.noreply.github.com>
2023-06-04 15:36:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 
-streamlit.header("Breakfast Menu").lstrip()
+streamlit.header("Breakfast Menu").strip()
 streamlit.text("Omega 3 & blueberry oatmeal")
 streamlit.text("Kale, Spinach & Rocket Smoothie")
 streamlit.text("Hard-boiled Free-Range Egg")
